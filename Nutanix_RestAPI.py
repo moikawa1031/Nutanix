ea6a3925678481503255068aa22d12f9f77ca42e
moikawa1031 <eternal-f@hotmail.co.jp>
2020-10-09 15:11:48 +0900

    Add files via upload

---
+++ Nutanix_RestAPI.py
@@ -0,0 +1,20 @@
+import requests, json, urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+IP = '172.29.161.60'
+USER = 'admin'
+PASSWORD = 'Peg-12345!'
+session = requests.Session()
+session.auth = (USER, PASSWORD)
+session.verify = False
+session.headers.update({'Content-Type': 'application/json; charset=utf-8'})
+
+url = f'https://{IP}:9440/PrismGateway/services/rest/v1/cluster'
+response = session.get(url)
+if response.ok:
+    j = response.json()
+    print(json.dumps(j, indent=2))
+else:
+    print('error happens')
+    print(f'sutatus code : {response.status_code}')
+    print(response.text)
